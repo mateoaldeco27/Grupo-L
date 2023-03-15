8a3d4e01c82186e0e828ff93c3249434af02efcc
PasteDev <matiasdoval123@gmail.com>
2023-03-14 21:05:26 -0300

    Simplificar el if y crear una variable global para Path

--- prueba1/src/Main.java
+++ prueba1/src/Main.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,19 +15,20 @@ public class Main {
         String contenido = "Hello world!";
         String contenido2 = "Creo que ya pueden ver este código!! No?";
 
-        Boolean archivo = Files.exists(Paths.get(textoNuevo));
+        final Path paths = Paths.get(textoNuevo);
+        boolean archivo = Files.exists(paths);
 
-        if (archivo == true) {
-            Files.writeString(Paths.get(textoNuevo), contenido + "\n\t"+ contenido2);
+        if (archivo) {
+            Files.writeString(paths, contenido + "\n\t"+ contenido2);
             System.out.println("El archivo "+ nombreArchivo +" existe y ya se le ha agregado el contenido. Echale una mirada!! :)");
             System.out.println("Has mirado el archivo?");
             scanner.nextLine();
             System.out.println("Espero que sí, porque ya no existe más!!!!! XDD");
-            Files.delete(Paths.get(textoNuevo));
+            Files.delete(paths);
         }
         else {
             System.out.println("El archivo "+ nombreArchivo +" no existe, pero descuida que ya ha se ha creado!! Vuelve a ejecutar el programa! :)");
-            Files.createFile(Paths.get(textoNuevo));
+            Files.createFile(paths);
         }
 
     }
