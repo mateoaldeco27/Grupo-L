79fdc7dc6c927a9d7b8cdbd970d243a10af5c30f
FranLencina <franlencinaa96@gmail.com>
2023-04-04 15:57:35 -0300

    Modificamos los for y reordenamos codigo
    ademas limpiamos comentarios que ya no servian y borramos codigo que no estaba en uso
    
    Co-Authored-By: Raúl Arriestra <127221487+ulrra2023@users.noreply.github.com>
    Co-Authored-By: ElKuzcu <127354832+ElKuzcu@users.noreply.github.com>
    Co-Authored-By: mateoaldeco27 <105385975+mateoaldeco27@users.noreply.github.com>
    Co-Authored-By: Shiryu180 <50419317+Shiryu180@users.noreply.github.com>

--- Entrega1/src/Main.java
+++ Entrega1/src/Main.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-//new
 import java.util.ArrayList;
 
 public class Main {
@@ -28,74 +27,74 @@ public class Main {
 
         //Extracción de datos del .csv y armado de objetos Equipo y Partido
         int puntos = 0;
-        for (int i = 1; i < resultadoDatos.length; i++) {
-            //Extracción de datos de la fila de resultados.csv
-            String[] infoPartido = resultadoDatos[i].split(";");
+        //agrego id
+        int id = 0;
+        for (int i = 1; i < pronosticoDatos.length; i++) {
 
-
-
-            //new
             String[] infoPronostico = pronosticoDatos[i].split(";");
 
-
-            //Armado de objetos equipo1, equipo2 y nuevoPartido, con base a las posiciones de las columnas de resultados.csv
-            Equipo equipo1 = new Equipo(Integer.parseInt(infoPartido[0]), infoPartido[1], infoPartido[2]);
-            Equipo equipo2 = new Equipo(Integer.parseInt(infoPartido[6]), infoPartido[5], infoPartido[7]);
-            Partido nuevoPartido = new Partido(equipo1, Integer.parseInt(infoPartido[3]), Integer.parseInt(infoPartido[4]), equipo2);
-
-            //new
-            partidos.add(nuevoPartido);
-
-
-            //new modified
-
-
-            nuevoPartido.mostrameDatosPartido(i);
-
-            //Extracción de datos de la fila, armado del objeto pronostico, y seteo de expectativa; con base al archivo pronostico.csv
-            Pronostico nuevoPronostico = new Pronostico(nuevoPartido, equipo1, equipo2);
-            //new modified
-            nuevoPronostico.ComprobarDatos(infoPronostico);
-
-
-            //new
-            for (int j = 1; j < pronosticoDatos.length; j++) {
-                //new
+            //creo participantes con if
+            if (id != Integer.parseInt(infoPronostico[0])){
                 Participante nuevoParticipante = new Participante(Integer.parseInt(infoPronostico[0]), infoPronostico[1], RESULTADO.ganador);
-
-                System.out.println(nuevoParticipante.getId());
-
-                //while (nuevoParticipante.getId() != ) {
-                //  participantes.add(nuevoParticipante);
-                //}
-
+                participantes.add(nuevoParticipante);
+                id++;
             }
 
+            //System.out.println("Id del participante i("+i+"): " + participantes.get(i).getId());
+            if (partidos.size() < 5 )
+            {
+                for (int j = 1; j < resultadoDatos.length; j++) {
+                    //Extracción de datos de la fila de resultados.csv
+                    String[] infoPartido = resultadoDatos[j].split(";");
 
-            //Resultados del partido y comparación con el pronóstico de Mariana.
-            /*
-            System.out.println("Goles Equipo 1 -> "+ nuevoPartido.getGoles1());
-            System.out.println("Goles Equipo 2 -> "+ nuevoPartido.getGoles2());
-            */
-            nuevoPartido.GanadorPartido(i);
-            nuevoPronostico.AcertoElPronostico();
-            System.out.println("\nEn este Partido Mariana obtuvo: " + nuevoPronostico.puntos + " punto/s.");
-            puntos = puntos + nuevoPronostico.puntos;
-            System.out.println("De momento Mariana tiene: " + puntos + " punto/s.\n");
-            //mostramos por consola la informacion
-            //mostrameInformacionPartidoPorConsola(i,equipo1,equipo2,nuevoPartido,nuevoPronostico,puntos);
+                    //String[] infoPronostico = pronosticoDatos[i].split(";");
+
+
+                    //Armado de objetos equipo1, equipo2 y nuevoPartido, con base a las posiciones de las columnas de resultados.csv
+                    Equipo equipo1 = new Equipo(Integer.parseInt(infoPartido[0]), infoPartido[1], infoPartido[2]);
+                    Equipo equipo2 = new Equipo(Integer.parseInt(infoPartido[6]), infoPartido[5], infoPartido[7]);
+                    Partido nuevoPartido = new Partido(equipo1, Integer.parseInt(infoPartido[3]), Integer.parseInt(infoPartido[4]), equipo2);
+
+
+                    //new
+                    if (!partidos.contains(nuevoPartido)) {
+
+                        partidos.add(nuevoPartido);
+
+
+                    }
+
+                    //System.out.println("---------------" + nuevoPartido.mostrameDatosPartido(j));
+
+
+                    //new modified
+
+                    nuevoPartido.mostrameDatosPartido(i);
+                    //Extracción de datos de la fila, armado del objeto pronostico, y seteo de expectativa; con base al archivo pronostico.csv
+                    Pronostico nuevoPronostico = new Pronostico(nuevoPartido, equipo1, equipo2);
+                    //new modified
+                    nuevoPronostico.ComprobarDatos(infoPronostico);
+
+
+                    nuevoPartido.GanadorPartido(i);
+                    nuevoPronostico.AcertoElPronostico();
+                    System.out.println("\nEn este Partido Mariana obtuvo: " + nuevoPronostico.puntos + " punto/s.");
+                    puntos = puntos + nuevoPronostico.puntos;
+                }
+            }
+
         }
 
         System.out.println("\nLos puntos totales de Mariana son: " + puntos);
-
-        //new
-        System.out.println(partidos.size());
-        System.out.println(participantes.size());
-
+/*
         for (Partido listaPartidos : partidos) {
             System.out.println(listaPartidos.getNombreEquipo1());
         }
+*/
+        System.out.println(partidos.size());
+        System.out.println("La cantidad de participantes son: " + participantes.size());
 
+        }
 
     }
-}
+
--- Entrega1/src/Partido.java
+++ Entrega1/src/Partido.java
@@ -1,17 +1,20 @@
-public class Partido {
-    public Partido (Equipo Equipo1, int goles1, int goles2, Equipo Equipo2){
+public class Partido
+{
+    public Partido(Equipo Equipo1, int goles1, int goles2, Equipo Equipo2)
+    {
         this.Equipo1 = Equipo1;
         this.Equipo2 = Equipo2;
         this.goles1 = goles1;
         this.goles2 = goles2;
     }
+
     private Equipo Equipo1;
     private Equipo Equipo2;
     private int goles1;
     private int goles2;
 
     //new delete
-    public String getNombreEquipo1 () {
+    public String getNombreEquipo1() {
         return Equipo1.GetNombre();
     }
 
@@ -27,24 +30,25 @@ public class Partido {
         if (goles1 > goles2) {
             Equipo1.SetResultado(RESULTADO.ganador);
             Equipo2.SetResultado(RESULTADO.perdedor);
-            System.out.println("Ganador Partido "+ i +": equipo 1");
+            System.out.println("Ganador Partido " + i + ": equipo 1");
 
         } else if (goles2 > goles1) {
             Equipo1.SetResultado(RESULTADO.perdedor);
             Equipo2.SetResultado(RESULTADO.ganador);
             //System.out.println("GanÃ³ el equipo 2");
-            System.out.println("Ganador Partido "+ i +": equipo 2");
+            System.out.println("Ganador Partido " + i + ": equipo 2");
         } else {
             Equipo1.SetResultado(RESULTADO.empate);
             Equipo2.SetResultado(RESULTADO.empate);
             //System.out.println("Empataron");
-            System.out.println("Ganador Partido "+ i +": empataron");
+            System.out.println("Ganador Partido " + i + ": empataron");
         }
     }
 
     //new modified
-    public void mostrameDatosPartido(int indice) {
-        System.out.println("En el partido " + indice + " jugaron:\n" +
+    public String mostrameDatosPartido(int indice)
+    {
+       return("En el partido " + indice + " jugaron:\n" +
                 "Equipo1: " + Equipo1.GetNombre() +
                 " | Equipo2: " + Equipo2.GetNombre() +
                 " | Goles1: " + goles1 +
